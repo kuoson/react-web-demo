6d402990dd6c0bba5256b7c08c25fca347f0f990
guosonqin <guosong.qin@szjinhuitech.com>
2025-05-15 16:54:52 +0800

    feat: 路由配置

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import React from 'react'
+import { RouterProvider } from 'react-router-dom'
+import router from '@/router'
+
 function App() {
-  return <></>
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  )
 }
 
 export default App
