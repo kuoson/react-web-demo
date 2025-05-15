c6d9e3dd48a63834a775fb2a3559b516f036f4d9
guosonqin <guosong.qin@szjinhuitech.com>
2025-05-15 15:35:12 +0800

    feat: layout 基础组件

---
+++ src/layouts/MainLayout.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import type { FC } from 'react'
+import { Outlet } from 'react-router-dom'
+
+const MainLayout: FC = () => {
+  return (
+    <>
+      <div>header</div>
+      <div>
+        <Outlet />
+      </div>
+      <div>footer</div>
+    </>
+  )
+}
+
+export default MainLayout
---
+++ src/layouts/ManageLayout.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import type { FC } from 'react'
+import { Outlet } from 'react-router-dom'
+import styles from './ManageLayout.module.scss'
+
+const ManageLayout: FC = () => {
+  return (
+    <>
+      <div className={styles.left}>left</div>
+      <div>
+        <Outlet />
+      </div>
+    </>
+  )
+}
+
+export default ManageLayout
