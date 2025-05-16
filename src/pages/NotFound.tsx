28abc1eee33d5ffe358fecf5fd8e468d7aa06acb
guosonqin <guosong.qin@szjinhuitech.com>
2025-05-16 17:13:39 +0800

    refactor: 404页改造

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,7 +1,23 @@
 import type { FC } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { Button, Result } from 'antd'
+import { HOME_PATHNAME } from '@/router/routes'
 
 const NotFound: FC = () => {
-  return <div>NotFound</div>
+  const nav = useNavigate()
+
+  return (
+    <Result
+      status="404"
+      title="404"
+      subTitle="Sorry, the page you visited does not exist."
+      extra={
+        <Button type="primary" onClick={() => nav(HOME_PATHNAME)}>
+          返回首页
+        </Button>
+      }
+    />
+  )
 }
 
 export default NotFound
