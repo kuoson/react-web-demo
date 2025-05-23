f09d6bba40d2a18a8a12627167e49edf15c413f9
guosonqin <guosong.qin@szjinhuitech.com>
2025-05-23 15:33:06 +0800

    feat: register 静态组件

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,7 +1,60 @@
 import type { FC } from 'react'
+import { Link } from 'react-router-dom'
+import { UserAddOutlined } from '@ant-design/icons'
+import { Typography, Space, Form, Input, Button } from 'antd'
+import { HOME_PATHNAME } from '@/router/routes'
+import styles from './Register.module.scss'
+
+const { Title } = Typography
 
 const Register: FC = () => {
-  return <div>Register</div>
+  const handleFinish = (vals: any) => {
+    console.log(vals)
+  }
+
+  return (
+    <div className={styles.wrapper}>
+      <div>
+        <Space>
+          <Title level={2}>
+            <UserAddOutlined />
+          </Title>
+          <Title level={2}>注册新用户</Title>
+        </Space>
+      </div>
+      <div>
+        <Form
+          name="basic"
+          labelCol={{ span: 6 }}
+          wrapperCol={{ span: 18 }}
+          style={{ maxWidth: 600 }}
+          autoComplete="off"
+          onFinish={handleFinish}
+        >
+          <Form.Item label="用户名" name="username">
+            <Input />
+          </Form.Item>
+          <Form.Item label="密码" name="password">
+            <Input.Password />
+          </Form.Item>
+          <Form.Item label="确认密码" name="confirmPassword">
+            <Input.Password />
+          </Form.Item>
+          <Form.Item label="昵称" name="nickname">
+            <Input />
+          </Form.Item>
+          <Form.Item label={null}>
+            <Space>
+              <Button type="primary" htmlType="submit">
+                注册
+              </Button>
+              <Link to={HOME_PATHNAME}>已有账户，请登录</Link>
+            </Space>
+          </Form.Item>
+        </Form>
+      </div>
+    </div>
+  )
 }
 
 export default Register
