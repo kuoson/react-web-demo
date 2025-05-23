aabe7a00b7f385744887f74a0bc6a3c2fc1b4d7b
guosonqin <guosong.qin@szjinhuitech.com>
2025-05-23 15:56:21 +0800

    feat: login "记住我" 功能

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,16 +1,53 @@
-import type { FC } from 'react'
+import { useEffect, type FC } from 'react'
 import { Link } from 'react-router-dom'
 import { UserOutlined } from '@ant-design/icons'
-import { Typography, Space, Form, Input, Button } from 'antd'
+import { Typography, Space, Form, Input, Button, Checkbox } from 'antd'
 import { REGISTER_PATHNAME } from '@/router/routes'
 import styles from './Login.module.scss'
 
 const { Title } = Typography
 
-const Login: FC = () => {
-  const handleFinish = (vals: any) => {
-    console.log(vals)
+const USERNAME_KEY = 'USERNAME'
+const PASSWORD_KEY = 'PASSWORD'
+
+const setUserInfoFromLocal = (username: string, password: string) => {
+  localStorage.setItem(USERNAME_KEY, username)
+  localStorage.setItem(PASSWORD_KEY, password)
+}
+
+const delUserInfoFromLocal = () => {
+  localStorage.removeItem(USERNAME_KEY)
+  localStorage.removeItem(PASSWORD_KEY)
+}
+
+const getUserInfoFromLocal = () => {
+  return {
+    username: localStorage.getItem(USERNAME_KEY),
+    password: localStorage.getItem(PASSWORD_KEY),
   }
+}
+
+const Login: FC = () => {
+  const [form] = Form.useForm()
+
+  const handleFinish = (vals: any) => {
+    const { username, password, remember } = vals
+
+    if (remember) {
+      setUserInfoFromLocal(username, password)
+    } else {
+      delUserInfoFromLocal()
+    }
+  }
+
+  useEffect(() => {
+    const { username, password } = getUserInfoFromLocal()
+
+    form.setFieldsValue({
+      username,
+      password,
+    })
+  }, [form])
 
   return (
     <div className={styles.wrapper}>
@@ -29,6 +66,8 @@ const Login: FC = () => {
           wrapperCol={{ span: 18 }}
           style={{ maxWidth: 600 }}
           autoComplete="off"
+          initialValues={{ remember: true }}
+          form={form}
           onFinish={handleFinish}
         >
           <Form.Item label="用户名" name="username">
@@ -36,6 +75,9 @@ const Login: FC = () => {
           </Form.Item>
           <Form.Item label="密码" name="password">
             <Input.Password autoComplete="new-password" />
+          </Form.Item>
+          <Form.Item name="remember" valuePropName="checked" label={null}>
+            <Checkbox>记住我</Checkbox>
           </Form.Item>
           <Form.Item label={null}>
             <Space>
