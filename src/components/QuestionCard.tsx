ab8e16af2668448fb600e5987b065ae8a7396719
guosonqin <guosong.qin@szjinhuitech.com>
2025-05-30 16:08:57 +0800

    feat: 删除功能联调

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -7,12 +7,15 @@ import {
   LineChartOutlined,
   CopyOutlined,
   DeleteOutlined,
+  ExclamationCircleFilled,
 } from '@ant-design/icons'
-import { Tag, Space, Divider, Button, message } from 'antd'
+import { Tag, Space, Divider, Button, message, Modal } from 'antd'
 import { useRequest } from 'ahooks'
 import { reqUpdateQuestion, reqDuplicateQuestion } from '@/api/question'
 import { QUESTION_EDIT_PATHNAME, QUESTION_STAT_PATHNAME } from '@/router/routes'
 import styles from './QuestionCard.module.scss'
+
+const { confirm } = Modal
 
 export type PropsType = {
   _id: string
@@ -28,6 +31,7 @@ const List: FC<PropsType> = (props: PropsType) => {
 
   const { _id, title, createdAt, answerCount, isPublished, isStar } = props
   const [curStar, setCurStar] = useState(isStar)
+  const [curDelete, setCurDelete] = useState(false)
 
   const { loading: changeStarLoading, run: handleChangeStar } = useRequest(
     async () => {
@@ -53,78 +57,110 @@ const List: FC<PropsType> = (props: PropsType) => {
       },
     })
 
+  const { loading: changeDelLoading, run: handleConfirmDel } = useRequest(
+    async () => {
+      await reqUpdateQuestion(_id, {
+        isDeleted: true,
+      })
+    },
+    {
+      manual: true,
+      onSuccess: () => {
+        message.success('删除成功')
+        setCurDelete(true)
+      },
+    },
+  )
+
+  const handleDel = () => {
+    confirm({
+      title: '确认删除该问卷?',
+      icon: <ExclamationCircleFilled />,
+      okText: '确认',
+      cancelText: '取消',
+      onOk: handleConfirmDel,
+    })
+  }
+
   return (
     <>
-      <div className={styles.wrapper}>
-        <div className={styles.top}>
-          <Link
-            to={
-              isPublished
-                ? `${QUESTION_STAT_PATHNAME}/${_id}`
-                : `${QUESTION_EDIT_PATHNAME}/${_id}`
-            }
-          >
+      {!curDelete && (
+        <div className={styles.wrapper}>
+          <div className={styles.top}>
+            <Link
+              to={
+                isPublished
+                  ? `${QUESTION_STAT_PATHNAME}/${_id}`
+                  : `${QUESTION_EDIT_PATHNAME}/${_id}`
+              }
+            >
+              <Space>
+                {curStar && <StarOutlined style={{ color: 'red' }} />}
+                {title}
+              </Space>
+            </Link>
             <Space>
-              {curStar && <StarOutlined style={{ color: 'red' }} />}
-              {title}
+              {isPublished ? (
+                <Tag color="processing">已发布</Tag>
+              ) : (
+                <Tag>未发布</Tag>
+              )}
+              <span>答卷: {answerCount}</span>
+              <span>{createdAt}</span>
             </Space>
-          </Link>
-          <Space>
-            {isPublished ? (
-              <Tag color="processing">已发布</Tag>
-            ) : (
-              <Tag>未发布</Tag>
-            )}
-            <span>答卷: {answerCount}</span>
-            <span>{createdAt}</span>
-          </Space>
+          </div>
+          <Divider style={{ margin: '12px 0px' }} />
+          <div className={styles.bottom}>
+            <Space>
+              <Button
+                icon={<EditOutlined />}
+                type="text"
+                onClick={() => {
+                  nav(`${QUESTION_EDIT_PATHNAME}/${_id}`)
+                }}
+              >
+                编辑问卷
+              </Button>
+              <Button
+                icon={<LineChartOutlined />}
+                type="text"
+                disabled={isPublished ? false : true}
+                onClick={() => {
+                  nav(`${QUESTION_STAT_PATHNAME}/${_id}`)
+                }}
+              >
+                问卷统计
+              </Button>
+            </Space>
+            <Space>
+              <Button
+                icon={<StarOutlined />}
+                type="text"
+                loading={changeStarLoading}
+                onClick={handleChangeStar}
+              >
+                {curStar ? '取消标星' : '标星'}
+              </Button>
+              <Button
+                icon={<CopyOutlined />}
+                type="text"
+                loading={changeDuplicateLoading}
+                onClick={handleDuplicateStar}
+              >
+                复制
+              </Button>
+              <Button
+                icon={<DeleteOutlined />}
+                type="text"
+                loading={changeDelLoading}
+                onClick={handleDel}
+              >
+                删除
+              </Button>
+            </Space>
+          </div>
         </div>
-        <Divider style={{ margin: '12px 0px' }} />
-        <div className={styles.bottom}>
-          <Space>
-            <Button
-              icon={<EditOutlined />}
-              type="text"
-              onClick={() => {
-                nav(`${QUESTION_EDIT_PATHNAME}/${_id}`)
-              }}
-            >
-              编辑问卷
-            </Button>
-            <Button
-              icon={<LineChartOutlined />}
-              type="text"
-              disabled={isPublished ? false : true}
-              onClick={() => {
-                nav(`${QUESTION_STAT_PATHNAME}/${_id}`)
-              }}
-            >
-              问卷统计
-            </Button>
-          </Space>
-          <Space>
-            <Button
-              icon={<StarOutlined />}
-              type="text"
-              loading={changeStarLoading}
-              onClick={handleChangeStar}
-            >
-              {curStar ? '取消标星' : '标星'}
-            </Button>
-            <Button
-              icon={<CopyOutlined />}
-              type="text"
-              loading={changeDuplicateLoading}
-              onClick={handleDuplicateStar}
-            >
-              复制
-            </Button>
-            <Button icon={<DeleteOutlined />} type="text">
-              删除
-            </Button>
-          </Space>
-        </div>
-      </div>
+      )}
     </>
   )
 }
