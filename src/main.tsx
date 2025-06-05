9d5663f601e00c6d14ac13d4f91570bb497c9906
guosonqin <guosong.qin@szjinhuitech.com>
2025-06-05 14:28:43 +0800

    feat: 重置浏览器样式

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux'
 import '@ant-design/v5-patch-for-react-19'
 import store from '@/store'
 import App from './App.tsx'
+import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
