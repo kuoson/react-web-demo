4fbe7da468d0527cf4bdbccb759d180faaf393a6
guosonqin <guosong.qin@szjinhuitech.com>
2025-06-06 12:02:19 +0800

    feat: edit 页面结构初始化

--- src/pages/question/Edit/index.tsx
+++ src/pages/question/Edit/index.tsx
@@ -1,7 +1,23 @@
 import type { FC } from 'react'
+import styles from './index.module.scss'
 
 const Edit: FC = () => {
-  return <div>Edit</div>
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.header}>Header</div>
+      <div className={styles['content-wrapper']}>
+        <div className={styles.content}>
+          <div className={styles.left}>left</div>
+          <div className={styles.main}>
+            <div className={styles['canvas-wrapper']}>
+              <div style={{ height: '900px' }}>画布</div>
+            </div>
+          </div>
+          <div className={styles.right}>right</div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Edit
