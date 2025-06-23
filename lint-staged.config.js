f2e3d792565b772d71bdeefc2864b8da9828a84d
guosonqin <guosong.qin@szjinhuitech.com>
2025-06-23 16:34:11 +0800

    chore: lint-staged 不需要 git add, 执行完后，会自动 git add && 命令需要 -- 接受文件传入，这样才不会对所有文件执行

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,3 +1,4 @@
 export default {
-  'src/**/*.{js,ts,jsx,tsx}': ['npm run format', 'npm run lint', 'git add'],
+  '*.{js,ts,jsx,tsx}': ['npm run format --', 'npm run lint:fix --'],
+  '*.{json,css,md}': 'npm run format --',
 }
